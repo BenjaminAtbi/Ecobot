3ad80d8fc698a836b8b709bf528a6cbadb86c68e
Muhammad Abid <abidsaleem682@gmail.com>
2019-02-21 16:25:22 -0800

    added the time frame as a parameter of the function

--- motor.py
+++ motor.py
@@ -2,21 +2,31 @@ from gpiozero import Robot, DistanceSensor
 from time import sleep
 
 class MotorControl:
-    def Cur_State(self, state):
+    def Cur_State(self, state, tf):
         motor1 = motor(4, 14)
         motor2 = motor(17, 18)
-        if state == 1:
+        
+        if state == 'f':
             motor1.forward()
             motor2.forward()
-        elif state == 2:
+            sleep(tf)
+        elif state == 'b':
             motor1.backward()
             motor2.backward()
-        elif state == 3:
+            sleep(tf)
+        elif state == 'l':
             motor1.left()
             motor2.left()
-        elif state == 4:
+            sleep(tf)
+        elif state == 'r':
             motor1.right()
             motor2.right()
+            sleep(tf)
+        elif state == 'u':
+            motor1.right()
+            motor2.left()
+            sleep(tf)
+
 
 
 
