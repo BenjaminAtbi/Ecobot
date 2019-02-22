c445fb26d509e08b85b625a211ddc45d0e69fc39
Muhammad Abid <abidsaleem682@gmail.com>
2019-02-21 18:31:37 -0800

    test other motors

--- testmortor.py
+++ testmortor.py
@@ -1,10 +1,10 @@
 from gpiozero import Robot, DistanceSensor
 from time import sleep
 
-motor1 = motor(4)
-motor2 = motor(17)
+motor1 = motor(4, 6)
+#motor2 = motor(17)
 
 motor1.forward()
 sleep(1)
-motor2.forward()
-sleep(1)
+#motor2.forward()
+#sleep(1)
