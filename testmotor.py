91d9b7f6b3e2a514268c3b54c3f51452e778a7a3
Muhammad Abid <abidsaleem682@gmail.com>
2019-02-21 18:37:59 -0800

    test other motors

--- testmotor.py
+++ testmotor.py
@@ -1,10 +1,10 @@
 from gpiozero import Motor
 from time import sleep
 
-motor1 = Motor(4, 6)
+motor1 = Motor(forward = 4, backward = GND)
 #motor2 = motor(17)
 
 motor1.forward()
-sleep(1)
+sleep(5)
 #motor2.forward()
 #sleep(1)
