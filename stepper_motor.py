67f1be4dd7065ac4dbf119ed11b9ebc12248cb48
Muhammad Abid <abidsaleem682@gmail.com>
2019-02-21 17:47:37 -0800

    fixed mistakes

--- stepper_motor.py
+++ stepper_motor.py
@@ -14,36 +14,36 @@ stepDir = -1        # Set to 1 for clockwise
 mode = 1            # mode = 1: Low Speed ==> Higher Power
                         # mode = 0: High Speed ==> Lower Power
 if mode:              # Low Speed ==> High Power
-seq = [ [1,0,0,1], # Define step sequence as shown in manufacturers datasheet
-        [1,0,0,0], 
-        [1,1,0,0],
-        [0,1,0,0],
-        [0,1,1,0],
-        [0,0,1,0],
-        [0,0,1,1],
-        [0,0,0,1] ]
+    seq = [ [1,0,0,1], # Define step sequence as shown in manufacturers datasheet
+            [1,0,0,0], 
+            [1,1,0,0],
+            [0,1,0,0],
+            [0,1,1,0],
+            [0,0,1,0],
+            [0,0,1,1],
+            [0,0,0,1] ]
 else:                    # High Speed ==> Low Power 
-seq = [ [1,0,0,0], # Define step sequence as shown in manufacturers datasheet
-        [0,1,0,0],
-        [0,0,1,0],
-        [0,0,0,1] ]
-        
+    seq = [ [1,0,0,0], # Define step sequence as shown in manufacturers datasheet
+            [0,1,0,0],
+            [0,0,1,0],
+            [0,0,0,1] ]
+
 stepCount = len(seq)
 if len(sys.argv)>1: # Read wait time from command line
-waitTime = int(sys.argv[1])/float(1000)
+    waitTime = int(sys.argv[1])/float(1000)
 else:
-waitTime = 0.004    # 2 miliseconds was the maximun speed got on my tests
+    waitTime = 0.004    # 2 miliseconds was the maximun speed got on my tests
 stepCounter = 0
 while True:                          # Start main loop
-for pin in range(0,4):
-    xPin=stepPins[pin]          # Get GPIO
-    if seq[stepCounter][pin]!=0:
-    xPin.on()
-    else:
-    xPin.off()
-stepCounter += stepDir
-if (stepCounter >= stepCount):
-    stepCounter = 0
-if (stepCounter < 0):
-    stepCounter = stepCount+stepDir
-time.sleep(waitTime)
+    for pin in range(0,4):
+        xPin=stepPins[pin]          # Get GPIO
+        if seq[stepCounter][pin]!=0:
+            xPin.on()
+        else:
+            xPin.off()
+    stepCounter += stepDir
+    if (stepCounter >= stepCount):
+        stepCounter = 0
+    if (stepCounter < 0):
+        stepCounter = stepCount+stepDir
+    time.sleep(waitTime)
