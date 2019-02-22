1f0bcbf15a5996e8a6841aa9b89db75578a0eef6
Muhammad Abid <abidsaleem682@gmail.com>
2019-02-21 20:39:22 -0800

    test other motors

--- testmotor.py
+++ testmotor.py
@@ -1,18 +1,21 @@
 from gpiozero import Servo
 from time import sleep
-myGPIO = 15
+myGPIO = 18
 #myGPIO2 = 22
 myServo = Servo(myGPIO)
 #myServo2 = Servo(myGPIO2)
 #motor2 = motor(17)
 while True:
-    
+    myServo.mid()
+    sleep(1)
     myServo.max()
     sleep(1)
-    
+    myServo.mid()
+    sleep(1)
     myServo.min()
     sleep(1)
-    
+    myServo.mid()
+    sleep(1)
     
     '''
     myServo2.max()
