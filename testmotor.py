7b8d3fe3eee57b6a73458e678e8fa08807b77171
Muhammad Abid <abidsaleem682@gmail.com>
2019-02-21 20:15:53 -0800

    test other motors

--- testmotor.py
+++ testmotor.py
@@ -4,14 +4,11 @@ from time import sleep
 myServo = Servo(4)
 #motor2 = motor(17)
 while True:
-    myServo.mid()
+    myServo.max()
     sleep(1)
     myServo.min()
     sleep(1)
-    myServo.mid()
-    sleep(1)
-    myServo.max()
-    sleep(1)
+    
     
 #motor2.forward()
 #sleep(1)
