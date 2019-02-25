2844308e06188743e15e60c11e6ea75b45905f11
Muhammad Abid <abidsaleem682@gmail.com>
2019-02-25 13:29:18 -0800

    test other motor

--- testmotor.py
+++ testmotor.py
@@ -12,11 +12,11 @@ myGPIO8 = 27 #classifier
 #rightWheel = Servo(myGPIO1)
 #leftWheel = Servo(myGPIO2)
 #miniFrontGate = Servo(myGPIO3)
-#rightPick = Servo(myGPIO4)
+rightPick = Servo(myGPIO4)
 leftPick = Servo(myGPIO5)
 
-#leftPick.value = 0
-
+leftPick.value = 0
+rightPick.value = 0
 '''
 rightWheel.min()
 leftWheel.min()
@@ -30,9 +30,11 @@ sleep(1)
 '''
 print("resting")
 leftPick.max()
+rightPick.max()
 sleep(2)
 print("max")
 
 leftPick.min()
+rightPick.min()
 sleep(2)
 print('min')
