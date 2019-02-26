2bad0257a2eec9dc47c61e3d29dca36075e28dcd
Muhammad Abid <abidsaleem682@gmail.com>
2019-02-25 16:44:00 -0800

    test other motor

--- testRPI.py
+++ testRPI.py
@@ -3,19 +3,19 @@ from time import sleep
 
 def SetAngle(angle):
 	duty = angle / 18 + 2
-	GPIO.output(04, True)
+	GPIO.output(4, True)
     GPIO.output(25, True)
 	pwm.ChangeDutyCycle(duty)
 	sleep(1)
-	GPIO.output(04, False)
+	GPIO.output(4, False)
     GPIO.output(25, True)
 	pwm.ChangeDutyCycle(0)
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(04, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
 GPIO.setup(25, GPIO.OUT)
 
-pwm=GPIO.PWM(04, 50)
+pwm=GPIO.PWM(4, 50)
 pwm2=GPIO.PWM(25, 50)
 pwm.start(0)
 pwm2.start(0)
